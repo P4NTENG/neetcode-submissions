03b3a3b734ddad0fff2a341af920fd325d602ebd
김인성 <32031234+P4NTENG@users.noreply.github.com>
2026-05-26 23:28:53 +0900

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        answer = 0
+        left = 0
+        most_freq = 0
+
+        for right in range(len(s)):
+            count[s[right]] = count.get(s[right], 0) + 1
+            most_freq = max(most_freq, count[s[right]])
+
+            while (right - left + 1) - most_freq > k:  # 나머지들을 k로 처리할 수 있는 동안
+                count[s[left]] = count.get(s[left], 0) - 1
+                left += 1
+
+            answer = max(answer, right - left + 1)
+        return answer
