7c5544499eceb98ad29d8968972a5598ca072b07
김인성 <32031234+P4NTENG@users.noreply.github.com>
2026-08-17 21:19:44 +0900

    [level 2] Title: 전화번호 목록, Time: 1030.21 ms, Memory: 51.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,31 @@
-def solution(phone_book):
-    phone_set = set(phone_book)
+class MyString:
+    def __init__(self, value):
+        self.value = value
 
-    for phone_number in phone_book:
+    def __hash__(self) -> int:
+        return hash(self.value)
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, MyString):
+            return self.value == other.value
+        if isinstance(other, str):
+            return self.value == other
+        return False
+
+    def __iter__(self):
+        yield from self.value
+
+    def __len__(self):
+        return len(self.value)
+
+    def __getitem__(self, key):
+        return MyString(self.value[key])
+
+
+def solution(phone_book):
+    phone_set = {MyString(phone) for phone in phone_book}
+
+    for phone_number in phone_set:
         for num in range(1, len(phone_number)):
             if phone_number[:num] in phone_set:
                 return False
